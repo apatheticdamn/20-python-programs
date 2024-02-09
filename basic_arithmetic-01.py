3b3fbaa8050b14dc51a02ea8536ddfdd1cb51385
Apathetic <135201180+apatheticdamn@users.noreply.github.com>
2024-02-09 22:23:12 +0530

    01

---
+++ basic_arithmetic-01.py
@@ -0,0 +1,12 @@
+first_num = float(input("Enter the first number: "))
+second_num = float(input("Enter the second number: "))
+
+sum = first_num + second_num
+difference = first_num - second_num
+product = first_num * second_num
+quotient = first_num / second_num
+
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
