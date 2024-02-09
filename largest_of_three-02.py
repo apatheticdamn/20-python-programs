885ab330eba4ddf23ac204f2d63632cdba78ef21
Apathetic <135201180+apatheticdamn@users.noreply.github.com>
2024-02-09 22:23:48 +0530

    02

---
+++ largest_of_three-02.py
@@ -0,0 +1,6 @@
+int1 = float(input("Enter the first number: "))
+int2 = float(input("Enter the second number: "))
+int3 = float(input("Enter the third number: "))
+
+max_value = max([int1, int2, int3])
+print(max_value)
