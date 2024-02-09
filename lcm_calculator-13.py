3d35fd92b274be1531d3b4c730f507ff1c7ce9c9
Apathetic <135201180+apatheticdamn@users.noreply.github.com>
2024-02-09 23:24:11 +0530

    fix: logical error

--- lcm_calculator-13.py
+++ lcm_calculator-13.py
@@ -3,10 +3,9 @@ num2 = int(input("Enter the second number: "))
 
 max_value = max(num1, num2)
 lcm = max_value
-if lcm % num1 == 0 and lcm % num2 == 0:
-    quit()
-lcm += max_value
-print("The lcm is", lcm)
 
-
-
+while True:
+    if lcm % num1 == 0 and lcm % num2 == 0:
+        break
+    lcm += max_value
+print("The lcm of {} and {} is {}".format(num1,num2,lcm))
