a1867acf2326ed57a1c4bfeceb9f2ca70fac67d1
Apathetic <135201180+apatheticdamn@users.noreply.github.com>
2024-02-09 22:24:04 +0530

    03

---
+++ sorting-03.py
@@ -0,0 +1,8 @@
+int1 = int(input("Enter the first number: "))
+int2 = int(input("Enter the second number: "))
+int3 = int(input("Enter the third number: "))
+
+sorted = sorted([int1, int2, int3])
+print(sorted)
+
+
